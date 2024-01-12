518f0f65bd1f9e068bbdf453b1e28d23782cfa56
Abhay Sahu <abhay1720sahu@gmail.com>
2024-01-12 11:31:04 +0530

    Update StudentController.java

--- StudentController.java
+++ StudentController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 @RestController
 @RequestMapping("/student")
-
+@CrossOrigin
 public class StudentController {
     @Autowired
     private StudentService studentService;
